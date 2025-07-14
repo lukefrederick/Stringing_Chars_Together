2bb996a5953c9c16b3edb5c992edf57cbca7c262
lukefrederick <129812402+lukefrederick@users.noreply.github.com>
2025-07-14 17:07:34 -0500

    Initial Commit

---
+++ CharChecker.js
@@ -0,0 +1,6 @@
+const input = require('readline-sync') ;
+
+let firstName = input.question("Enter your first name: ");
+let index = input.questionInt("Enter an index for the string: ");
+
+console.log("The character at index " + index + " is " + firstName[index])
