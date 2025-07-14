cfb34d03ccdf0c672735d2fa94cf8f9c84dfe3d9
lukefrederick <lfrederick220@gmail.com>
2025-07-14 17:12:21 -0500

    Bug corrected

--- CharChecker.js
+++ CharChecker.js
@@ -3,4 +3,4 @@ const input = require('readline-sync') ;
 let firstName = input.question("Enter your first name: ");
 let index = input.questionInt("Enter an index for the string: ");
 
-console.log("The character at index " + index + " is " + firstName[index])
+console.log("The character at index " + index + " is " + firstName[index]);
